574235a0db29d7b79ff46e2e3bb7d4c7b64569c8
Roberts Zalitis <dont@have.it>
2024-06-10 22:57:58 +0300

    Added php-cs-fixer to the project with config file

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+$finder = PhpCsFixer\Finder::create()
+    ->in(__DIR__)
+    ->exclude('vendor')
+    ->name('*.php')
+    ->notName('*.blade.php')
+    ->ignoreDotFiles(true)
+    ->ignoreVCS(true);
+
+$config = new PhpCsFixer\Config();
+return $config->setRules([
+    '@PSR2' => true,
+    'array_syntax' => ['syntax' => 'short'],
+    'ordered_imports' => ['sort_algorithm' => 'alpha'],
+    'no_unused_imports' => true,
+])
+    ->setFinder($finder);
